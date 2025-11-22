139d1decaa812f40b9fbc18e1a2860f619bd644a
usmanbankagmail <usmanbanka@gmail.com>
2025-11-22 13:49:41 +0500

    Added 3rd project which has inline styling practice

---
+++ jsx-practice-beginner-3/src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+
+  const myName = "Muhammad Usman Banka";
+  const myAge = 30;
+
+
+  return(
+    <>
+    <h1>My name is: {myName}</h1>
+    <h2 className = "digits profile">My age is: {myAge}</h2>
+    <h3  style = {{color: "red", fontSize: "20px"}}>This line uses inline Style for practicing / demonstration purpose</h3>
+    </>
+  );
+}
+
+export default App;
